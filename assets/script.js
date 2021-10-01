8190b04b607c24ea89a81328ca960d02c7697fe7
Sander Tiekstra <sandertiekstra@gmail.com>
2021-10-01 16:54:28 +0200

    Styling Living documentation

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,71 @@ function __init(root, git) {
     const path = window.location.href.substr(base_url.length);
     const root_url = git.is_feature_branch ? new URL(`../`, base_url).href : base_url;
 
-    const menu = document.getElementById("git-branch-menu");    
-    menu
-        .onclick = async () => {
-            const items_el = document.getElementById("git-branch-menu-items");
-            if(items_el != undefined) {
-                items_el.style.visibility = items_el.style.visibility == "visible" ? "hidden" : "visible";
-                return;
-            }
+    const loadBranches = async (menu) => {
+        menu.classList.add("loading");
 
-            menu.classList.add("loading");
+        const branches = await getBranches(`${root_url}branches.json`) ?? git.branches;
 
-            const branches = await getBranches(`${root_url}branches.json`) ?? git.branches;
+        const ul = document.createElement("ul");
 
-            const ul = document.createElement("ul");
-            ul.id = "git-branch-menu-items";
+        for (let branch of branches) {
+            branch.base_url = `${root_url}${branch.path === "" ? "" : branch.path + "/"}`;
+            branch.url = `${branch.base_url}${path}`;
+            branch.url_exists = await urlExists(branch.url);
 
-            for (let branch of branches) {
-                branch.base_url = `${root_url}${branch.path === "" ? "" : branch.path + "/"}`;
-                branch.url = `${branch.base_url}${path}`;
-                branch.url_exists = await urlExists(branch.url);
+            const li = document.createElement("li");
 
-                const li = document.createElement("li");
-                
-                const a = document.createElement("a");
-                a.href = branch.url_exists ? branch.url : `${branch.base_url}index.html`;
-                a.innerText = branch.name;
+            const a = document.createElement("a");
+            a.href = branch.url_exists ? branch.url : `${branch.base_url}index.html`;
+            a.innerText = branch.name;
 
-                li.appendChild(a);
-                
-                ul.appendChild(li);
-            }
+            li.appendChild(a);
 
-            menu.appendChild(ul);
-            
-            menu.classList.remove("loading");
-        };
+            ul.appendChild(li);
+        }
+
+        menu.innerHTML = '';
+        menu.appendChild(ul);
+
+        menu.classList.remove("loading");
+    };
+
+    const toggleMenu = (button, expand) => {
+        const menuId = button.hash;
+        const target = document.querySelector(menuId);
+        let expanded = button.getAttribute('aria-expanded') === 'true';
+
+        if (typeof expand == "boolean") {
+            expanded = !expand;
+        }
+
+        if (menuId === "#git-branch-menu") {
+            loadBranches(target);
+        }
+
+        button.setAttribute('aria-expanded', !expanded);
+        target.setAttribute('aria-hidden', expanded);
+
+        return !expanded;
+    }
+
+    const menuToggleButtons = document.querySelectorAll(".menu-toggle");
+
+    menuToggleButtons.forEach(btn => {
+        btn
+            .onclick = event => {
+                const toggle = event.target.closest('a');
+                const toggled = toggleMenu(toggle);
+
+                if (toggled) {
+                    const [sibling] = [...menuToggleButtons].filter(menuToggleButton => menuToggleButton !== toggle);
+                    toggleMenu(sibling, false);
+                }
+
+                event.preventDefault();
+            };
+    });
+
 };
 
 const urlExists = url => new Promise((resolve, reject) => {
@@ -87,3 +116,5 @@ async function getBranches(url) {
         return null
     }
 }
+
+iFrameResize({}, 'iframe');
