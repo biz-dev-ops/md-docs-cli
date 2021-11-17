3aaf724ab93a691baa0ddff01bf5ddcc4097b7c4
Sander Tiekstra <sandertiekstra@gmail.com>
2021-11-17 10:54:31 +0100

    #12: Scenario's accordion

--- assets/feature/script.js
+++ assets/feature/script.js
@@ -6,6 +6,28 @@ function __init(schema) {
     const html = Mustache.render(TEMPLATE, { data: data }, PARTIALS);
 
     document.body.insertAdjacentHTML("beforeend", html);
+
+    // Scenarios accordion
+    const scenarios = document.querySelectorAll('.scenario');
+    scenarios.forEach(scenario => {
+        const toggle = scenario.querySelector('button.item');
+        const steps = scenario.querySelector('.steps');
+
+        steps.setAttribute('hidden',  'hidden');
+        toggle.setAttribute('aria-expanded', 'false');
+
+        toggle.onclick = e => {
+            const expanded = toggle.getAttribute('aria-expanded') !== "false";
+            if (expanded) {
+                steps.setAttribute('hidden',  'hidden');
+                toggle.setAttribute('aria-expanded', 'false');
+            } else {
+                steps.removeAttribute('hidden');
+                toggle.setAttribute('aria-expanded', 'true');
+            }
+
+        };
+    })
 };
 
 function transformToMustacheData(schema) {
@@ -64,12 +86,14 @@ const TEMPLATE = `{{#data}}
 <ul class="scenarios">
     {{#scenarios}}
         <li class="scenario {{status.type}}">
-            <span>{{name}}</span>
+            <button class="item" aria-expanded="true">{{name}}</button>
 
             <ul class="steps">
                 {{#steps}}
                     <li class="step {{type}} {{status.type}}">
-                        <span class="keyword">{{keyword}}</span> <span>{{text}}</span>
+                        <span class="item">
+                            <span class="keyword">{{keyword}}</span> <span>{{text}}</span>
+                        </span>
                     </li>
                 {{/steps}}
             </ul>
