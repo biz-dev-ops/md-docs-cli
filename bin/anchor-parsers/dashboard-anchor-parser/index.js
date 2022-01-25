52ae9affd0c6108acfbdac581736b4d5a834df0a
Arjan Geertsema <arjan@synion.nl>
2022-01-25 10:35:14 +0100

    added composite features

--- bin/anchor-parsers/dashboard-anchor-parser/index.js
+++ bin/anchor-parsers/dashboard-anchor-parser/index.js
@@ -1,11 +1,10 @@
 const fs = require('fs').promises;
 const { env } = require('process');
 const chalk = require('chalk-next');
-const yaml = require('js-yaml');
-const path = require('path');
 
 const AnchorParser = require('../anchor-parser');
 
+const compositeParser = require('../../utils/bdd/composite-feature-parser');
 const gherkin = require('../../utils/bdd/gherkin-parser');
 const specflow = require('../../utils/bdd/specflow-test-executions-parser');
 const summarizer = require('../../utils/bdd/features-summarizer');
@@ -20,12 +19,18 @@ module.exports = class DasboardAnchorParser extends AnchorParser {
 
   _canParse(anchor) { return anchor.href.endsWith('.dashboard.yml') || anchor.href.endsWith('.dashboard.yaml'); }
 
-  async _parse(anchor, file) {
-    const directory = path.dirname(file);
-    const config = await  this.#parseFile(file);
+  async _parse(anchor, file) {    
+    const files = await compositeParser.parse(file);
+
+    if (env.NODE_ENV === 'development')
+      await fs.writeFile(`${file}.files.json`, JSON.stringify(files));
 
     console.info(chalk.green(`\t\t\t\t* parsing feature files`));
-    const features = await gherkin.parse(config.features.map(feature => path.resolve(directory, feature)));
+    const features = await gherkin.parse(files);
+    
+    if (env.NODE_ENV === 'development')
+      await fs.writeFile(`${file}.features.json`, JSON.stringify(features));
+    
     const executions = await this.testExecutionParser.get();
 
     console.info(chalk.green(`\t\t\t\t* parsing executions file`));
@@ -42,11 +47,5 @@ module.exports = class DasboardAnchorParser extends AnchorParser {
 
     console.info(chalk.green(`\t\t\t\t* rendering`));
     return this.component.render({ summary, features });
-  }
-
-  async #parseFile(file) {
-    const content = await this._readFileAsString(file);
-    const json = yaml.load(content);
-    return json;
   }
 };
--- bin/anchor-parsers/features-anchor-parser/index.js
+++ bin/anchor-parsers/features-anchor-parser/index.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 const { env } = require('process');
-const yaml = require('js-yaml');
 const chalk = require('chalk-next');
-const path = require('path');
 
 const AnchorParser = require('../anchor-parser');
 
+const compositeParser = require('../../utils/bdd/composite-feature-parser');
 const gherkin = require('../../utils/bdd/gherkin-parser');
 const specflow = require('../../utils/bdd/specflow-test-executions-parser');
+
 
 module.exports = class FeaturesAnchorParser extends AnchorParser {
   constructor({ options, testExecutionParser, featureComponent }) {
@@ -21,27 +21,27 @@ module.exports = class FeaturesAnchorParser extends AnchorParser {
 
   async _parse(anchor, file) {
     console.info(chalk.green(`\t\t\t\t* parsing file`));
-    const directory = path.dirname(file);
-    const config = await this.#parseFile(file);
-    const files = config.map(feature => path.resolve(directory, feature));
+    
+    const files = await compositeParser.parse(file);
+
+    if (env.NODE_ENV === 'development')
+      await fs.writeFile(`${file}.files.json`, JSON.stringify(files));
 
     console.info(chalk.green(`\t\t\t\t* parsing ${files.length} features`));
     const features = await gherkin.parse(files);
+
+    if (env.NODE_ENV === 'development')
+      await fs.writeFile(`${file}.features.json`, JSON.stringify(features));
+
     const executions = await this.testExecutionParser.get();
     
     console.info(chalk.green(`\t\t\t\t* parsing executions file`));    
     specflow.parse(features, executions);
     
     if (env.NODE_ENV === 'development')
-      await fs.writeFile(`${file}.json`, JSON.stringify(features));
+      await fs.writeFile(`${file}.features.json`, JSON.stringify(features));
 
     console.info(chalk.green(`\t\t\t\t* rendering`));
     return this.component.render({ features });
-  }
-
-  async #parseFile(file) {
-    const content = await this._readFileAsString(file);
-    const json = yaml.load(content);
-    return json;
   }
 };
