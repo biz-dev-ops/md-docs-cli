686893541277a29ee21d3db1524a7c4ab1e1eef9
Sander Tiekstra <sandertiekstra@gmail.com>
2022-03-09 12:07:36 +0100

    Optioneel logo

--- bin/components/page-component/index.js
+++ bin/components/page-component/index.js
@@ -1,12 +1,15 @@
 const pug = require('pug');
 const files = require('../../utils/files');
+const chalk = require("chalk-next");
 
 module.exports = class PageComponent {
-    constructor({ pageComponentRenderFn }) {
+    constructor({ options, pageComponentRenderFn }) {
         this.renderFn = pageComponentRenderFn ?? pug.compile(files.readFileAsStringSync(`${__dirname}/template.pug`));
+        this.options = options;
     }
-    
+
     render(data) {
-        return this.renderFn(data);
+        console.log(chalk.red(this.options.logo));
+        return this.renderFn({...data, logo: this.options.logo});
     }
 }
