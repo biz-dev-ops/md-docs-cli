4ddea1813bfc1167ed23f547bb62d7da615263f7
Arjan Geertsema <arjan@synion.nl>
2022-03-21 09:38:00 +0100

    fixed definition bug

--- bin/utils/definitions/definition-parser/index.js
+++ bin/utils/definitions/definition-parser/index.js
@@ -7,28 +7,41 @@ module.exports = class DefinitionParser {
         const defintions = await this.defintionStore.get();
 
         for (const definition of defintions) {
-            let replacement = '$1';
-            
-            if (definition.text)
-                replacement = `<abbr title="${definition.text}">${replacement}</abbr>`;
-            
-            if(definition.link)
-            {
-                let url = definition.link;
-                if (!url.startsWith('http'))
-                    url = root + url;
-                
-                replacement = `<a href="${url}">${replacement}</a>`
-            }
+            const regex = new RegExp(`(?![^<]*>)(${createAlias(definition)})`, 'img');
 
-            const alias = [ definition.name ];
-            if (definition.alias)
-                alias.push(definition.alias);
-
-            const regex = new RegExp(`(?![^<]*>)(${alias.join('|')})`, 'img');
-            html = html.replace(regex, replacement);
+            html = html.replace(regex, createReplacement(definition, root));
         }
 
         return html;
     }
+}
+
+function createReplacement(definition, root) {
+    let replacement = '$1';
+            
+    if (definition.text)
+        replacement = `<abbr title="${definition.text}">${replacement}</abbr>`;
+    
+    if(definition.link)
+    {
+        let url = definition.link;
+        if (!url.startsWith('http'))
+            url = root + url;
+        
+        replacement = `<a href="${url}">${replacement}</a>`
+    }
+    
+    return replacement;
+}
+
+function createAlias(definition) {
+    const aliasses = [ definition.name ];
+    if (definition.alias)
+        aliasses.push(...definition.alias);
+    
+    const alias = aliasses
+        .map(a => a.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
+        .join('|');
+    
+    return alias;
 }
