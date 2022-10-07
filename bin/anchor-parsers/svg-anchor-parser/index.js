8c7a7b26603e68b5059839cfd30e2e1947838652
Arjan Geertsema <arjan@synion.nl>
2022-10-07 23:00:59 +0200

    wip

--- bin/anchor-parsers/svg-anchor-parser/index.js
+++ bin/anchor-parsers/svg-anchor-parser/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const colors = require('colors');
-const files = require('../../utils/files');
 
 const AnchorParser = require('../anchor-parser');
 
@@ -11,15 +10,11 @@ module.exports = class ImageAnchorParser extends AnchorParser {
     this.root = options.dst;
   }
 
-  _canParse(anchor) { path.basename(anchor.href).endsWith('.svg') ; }
+  _canParse(anchor) { return path.basename(anchor.href).endsWith('.svg'); }
 
   async _parse(anchor, file) {
     console.info(colors.green(`\t\t\t\t* creating svg tag ${path.relative(this.root, file)}`));
     
-    const content = (await files.readFileAsString(file));
-
-    console.log(content)
-
-    return content;
+    return `<embed type="image/svg+xml" src='${anchor.href}' alt='${anchor.text}' />`;
   } 
 };
