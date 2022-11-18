60d8f57b91d353dec3d1e38e2838231d327a98d2
Sander Tiekstra <sandertiekstra@gmail.com>
2022-11-18 09:50:28 +0100

    Add array, object array and on-of form behavior

--- assets/scripts/user-task.js
+++ assets/scripts/user-task.js
@@ -1,97 +1,109 @@
 (() => {
-  document.querySelectorAll('.ui .action-list').forEach((actionList) => {
-    const actionCards = [];
+  document.querySelectorAll('.ui').forEach((userTask) => {
+    const initActionList = (actionList) => {
+      const actionCards = [];
 
-    const hide = (el) => {
-      el.setAttribute('hidden', 'hidden');
-    }
-
-    const show = (el) => {
-      el.removeAttribute('hidden');
-    }
-
-    actionList.querySelectorAll('button').forEach((action) => {
-      const targetId = action.getAttribute('aria-controls');
-      const target = document.getElementById(targetId);
-
-      actionCards.push(target);
-
-      action.onclick = event => {
-        show(target);
-        hide(actionList);
-      }
-    })
-
-    const closeActionCard = (card) => {
-      show(actionList);
-      hide(card);
-    }
-
-    actionCards.forEach(card => {
-      const form = card.querySelector('form');
-      const submit = card.querySelector('[type="submit"]');
-      const cancel = card.querySelector('[data-cancel]');
-
-      submit.onclick = (event) => {
-        const confirmText = submit.getAttribute("data-confirm");
-
-        if (form.checkValidity()) {
-          if (window.confirm(confirmText || "")) {
-            closeActionCard(card);
-          }
-        }
-
-        event.preventDefault();
+      const hide = (el) => {
+        el.setAttribute('hidden', 'hidden');
       }
 
-      cancel.onclick = (event) => {
-        closeActionCard(card);
-
-        event.preventDefault();
+      const show = (el) => {
+        el.removeAttribute('hidden');
       }
-    });
-  })
 
-  document.querySelectorAll('.ui .fieldset-controls').forEach((fieldsetControls) => {
-    const formItemTemplate = fieldsetControls.parentElement.querySelector('.form-item').cloneNode(true);
+      actionList.querySelectorAll('button').forEach((action) => {
+        const targetId = action.getAttribute('aria-controls');
+        const target = document.getElementById(targetId);
 
-    fieldsetControls.querySelectorAll('button').forEach((button) => {
-      const action = button.dataset.fieldsetAction;
-      button.addEventListener('click', () => {
-        if (action === 'add') {
-          insertBefore(formItemTemplate.cloneNode(true), fieldsetControls);
-        }
+        actionCards.push(target);
 
-        if (action === 'remove') {
-          const formItems = fieldsetControls.parentElement.querySelectorAll(':scope > .form-item');
-          if (formItems.length > 1) {
-            formItems[formItems.length - 1].remove();
-          }
+        action.onclick = event => {
+          show(target);
+          hide(actionList);
         }
       })
-    })
-  })
 
-  document.querySelectorAll('select.select-one-of').forEach(select => {
-    const formItems = select.parentNode.querySelectorAll(':scope > .form-item');
-    const showFormItem = index => {
-      formItems.forEach(item => {
-        item.setAttribute('hidden', true);
-      });
-
-      if (index && formItems[index]) {
-        formItems[index].removeAttribute('hidden');
+      const closeActionCard = (card) => {
+        show(actionList);
+        hide(card);
       }
+
+      actionCards.forEach(card => {
+        const form = card.querySelector('form');
+        const submit = card.querySelector('[type="submit"]');
+        const cancel = card.querySelector('[data-cancel]');
+
+        submit.onclick = (event) => {
+          const confirmText = submit.getAttribute("data-confirm");
+
+          if (form.checkValidity()) {
+            if (window.confirm(confirmText || "")) {
+              closeActionCard(card);
+            }
+          }
+
+          event.preventDefault();
+        }
+
+        cancel.onclick = (event) => {
+          closeActionCard(card);
+
+          event.preventDefault();
+        }
+      });
+    };
+
+    const initFieldSetControls = (fieldsetControls) => {
+      const formItemTemplate = fieldsetControls.parentElement.querySelector('.form-item').cloneNode(true);
+
+      fieldsetControls.querySelectorAll('button').forEach((button) => {
+        const action = button.dataset.fieldsetAction;
+        button.addEventListener('click', () => {
+          if (action === 'add') {
+            fieldsetControls.insertAdjacentElement('beforebegin', formItemTemplate.cloneNode(true));
+          }
+
+          if (action === 'remove') {
+            const formItems = fieldsetControls.parentElement.querySelectorAll(':scope > .form-item');
+            if (formItems.length > 1) {
+              formItems[formItems.length - 1].remove();
+            }
+          }
+        })
+      })
     }
 
-    select.addEventListener('change', () => {
-      showFormItem(select.value);
+    const initSelectOneOf = (select) => {
+      const formItems = select.closest('fieldset').querySelectorAll(':scope > .form-item');
+      const showFormItem = index => {
+        formItems.forEach(item => {
+          item.setAttribute('hidden', true);
+        });
+
+        if (index && formItems[index]) {
+          formItems[index].removeAttribute('hidden');
+        }
+      }
+
+      select.addEventListener('change', () => {
+        showFormItem(select.value);
+      });
+
+      showFormItem();
+    }
+
+    userTask.querySelectorAll('.action-list').forEach(initActionList);
+    userTask.querySelectorAll('.fieldset-controls').forEach(initFieldSetControls);
+    userTask.querySelectorAll('select.select-one-of').forEach(initSelectOneOf);
+
+    const observer = new MutationObserver(function(mutations_list) {
+      mutations_list.forEach(function(mutation) {
+        mutation.addedNodes.forEach(node => {
+          node.querySelectorAll('select.select-one-of').forEach(initSelectOneOf);
+        });
+      });
     });
 
-    showFormItem();
-  });
-
-  function insertBefore(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode);
-  }
+    observer.observe(userTask, { subtree: true, childList: true });
+  })
 })();
