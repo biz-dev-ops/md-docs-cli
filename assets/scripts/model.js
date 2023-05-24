0e0195ca9116851c947265a0641b7a24bbf03e49
Arjan Geertsema <arjan@synion.nl>
2023-05-24 17:05:40 +0200

    added json togle and array functionality

--- assets/scripts/model.js
+++ assets/scripts/model.js
@@ -1,3 +1,9 @@
 (() => {
+    document.querySelectorAll('.json-toggle').forEach(el => {
+        el.onclick = (event) => {
+            event.preventDefault();
 
+            event.target.nextElementSibling.toggleAttribute("hidden");
+        }
+    });
 })();
