461c88c94ffd43ddecd2d0881a5e45c3f4d2d3a1
Sander Tiekstra <sandertiekstra@gmail.com>
2023-07-14 13:59:38 +0200

    Remove deprecated model.js

--- assets/scripts/model.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    document.querySelectorAll('.json-toggle').forEach(el => {
-        el.onclick = (event) => {
-            event.preventDefault();
-
-            event.target.nextElementSibling.toggleAttribute("hidden");
-        }
-    });
-})();
