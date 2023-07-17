96f6b9c250eadaa68e9d3ddbb6511411f5a8f2bc
Arjan Geertsema <arjan@synion.nl>
2023-07-17 10:54:34 +0200

    added uuid id

--- bin/anchor-parsers/model-anchor-parser/index.js
+++ bin/anchor-parsers/model-anchor-parser/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 const { env } = require('process');
 const colors = require('colors');
+const { v4: uuidv4 } = require('uuid');
 
 const jsonSchemaParser = require('../../utils/json-schema-parser');
 
@@ -23,8 +24,10 @@ module.exports = class ModelAnchorParser extends AnchorParser {
     if (env.NODE_ENV === 'development')
       await fs.writeFile(`${file}.json`, JSON.stringify(json));
 
+    const id = `model-container-${uuidv4()}`;
+
     console.info(colors.green(`\t\t\t\t* rendering`));
-    const html = this.component.render({ json });
+    const html = this.component.render({ id, json });
 
     if (env.NODE_ENV === 'development')
       await fs.writeFile(`${file}.html`, html);
