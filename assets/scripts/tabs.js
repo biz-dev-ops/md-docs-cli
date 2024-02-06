9556237f372e89bdd3f023f4409344513d1747ce
Arjan Geertsema <arjan@synion.nl>
2024-02-06 07:58:21 +0100

    fixed bug

--- assets/scripts/tabs.js
+++ assets/scripts/tabs.js
@@ -1,19 +1,19 @@
 (() => {
-    document.querySelectorAll('.tabs-container').forEach((container) => {
-        const anchors = container.querySelectorAll(':scope .tabs-list-container a');
-        const panels = container.querySelectorAll(':scope .tabs-panels-container > div');
+    document.querySelectorAll(".tabs-container").forEach((container) => {
+        const anchors = container.querySelectorAll(":scope .tabs-list-container a");
+        const panels = container.querySelectorAll(":scope .tabs-panels-container > div");
 
         anchors.forEach((anchor, index) => {
             anchor.onclick = (event) => {
                 event.preventDefault();
 
-                anchors.forEach(sibling => sibling.removeAttribute('aria-expanded'));
-                anchor.setAttribute('aria-expanded', 'true');
+                anchors.forEach(sibling => sibling.removeAttribute("aria-expanded"));
+                anchor.setAttribute("aria-expanded", "true");
             
-                panels.forEach(sibling => sibling.setAttribute('hidden', 'true'));
+                panels.forEach(sibling => sibling.setAttribute("hidden", "true"));
 
-                panels[index].removeAttribute('hidden');
-                panels[index].dispatchEvent(new Event('visible'));
+                panels[index].removeAttribute("hidden");
+                panels[index].dispatchEvent(new Event("visible"));
             };
         })
     })
