fc681e0bc3852332dc957431020bca4e7222956e
Arjan Geertsema <arjan@synion.nl>
2024-02-08 10:52:55 +0100

    fixed bug

--- assets/scripts/iframe.js
+++ assets/scripts/iframe.js
@@ -1,3 +1,15 @@
 (() => {
-    iFrameResize({ }, 'iframe');
+    const resizedCollection = iFrameResize({}, "iframe");
+
+    if(resizedCollection.length > 0) {
+        window.addEventListener("ariaExpanded", (event) => {
+            const container = event.detail.target;
+            const iframe = container.querySelector("iframe");
+            if(!iframe.iFrameResizer) {
+                return;
+            }
+            
+            iframe.iFrameResizer.resize();
+        });
+    }
 })();
