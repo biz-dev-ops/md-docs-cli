9bab96a447ce735670fd622505d3533279f86201
Arjan Geertsema <arjan@synion.nl>
2024-10-28 20:11:48 +0100

    removed commented code"

--- assets/scripts/iframe.js
+++ assets/scripts/iframe.js
@@ -1,17 +1,3 @@
 (() => {
     iframeResize({ license: "GPLv3" });
-    
-    // const resizedCollection = iFrameResize({}, "iframe");
-
-    // if(resizedCollection.length > 0) {
-    //     window.addEventListener("ariaExpanded", (event) => {
-    //         const container = event.detail.target;
-    //         const iframe = container.querySelector("iframe");
-    //         if(!iframe.iFrameResizer) {
-    //             return;
-    //         }
-            
-    //         iframe.iFrameResizer.resize();
-    //     });
-    // }
 })();
